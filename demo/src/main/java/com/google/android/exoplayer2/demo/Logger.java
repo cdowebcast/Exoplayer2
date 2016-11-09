e26e4f9c7b8e91b01f239f1911ee443881cbaf7f
lleejong <lleejong@gmail.com>
2016-11-09 23:54:39 +0900

    initial commit

---
+++ demo/src/main/java/com/google/android/exoplayer2/demo/Logger.java
@@ -0,0 +1,16 @@
+package com.google.android.exoplayer2.demo;
+
+import android.util.Log;
+
+/**
+ * Created by lleejong on 2016. 11. 9..
+ */
+
+public class Logger {
+
+
+
+    public static void log(String where, String log){
+        Log.d("[LLEEJ,"+where+"]", log);
+    }
+}
